7098280429f691e0a717a9acd359cffe81ee2194
StruggleYang <1724555319@qq.com>
2017-10-25 17:35:14 +0800

    加入模板引擎

--- src/main/java/org/struy/main/web/MainControoller.java
+++ src/main/java/org/struy/main/web/MainControoller.java
@@ -1,13 +1,23 @@
 package org.struy.main.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class MainControoller {
 
-    @RequestMapping("/hello")
-    public String hello() {
+    @RequestMapping()
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping("/showFiles")
+    @ResponseBody
+    public String showFiles() {
+
         return "Hello";
     }
 
